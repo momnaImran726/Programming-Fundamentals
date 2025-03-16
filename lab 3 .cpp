1698ff7d47ad12c6356a6447c59d1f94128908a0
momnaImran726 <mominaahimran@gmail.com>
2025-03-16 13:15:58 +0500

    Add files via upload

---
+++ lab 3 .cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{	 
+//
+//	double item1=12.95;
+//	double item2=24.95;
+//	double item3=6.95;
+//	double item4=14.95;
+//	double item5=3.95;
+//	double subtotal= item1+item2+item3+item4+item5;
+//	double saletax=subtotal/100*6;
+//	cout<<"price of item 1 : $"<<item1<<endl;
+//	cout<<"price of item 2 : $"<<item2<<endl;
+//	cout<<"price of item 3 : $"<<item3<<endl;
+//	cout<<"price of item 4 : $"<<item4<<endl;
+//	cout<<"price of item 5 : $"<<item5<<endl;
+//	cout<<"sobtotal : $"<<subtotal<<endl;
+//	cout<<"sale tax (6%) :"<<saletax<<endl;
+//	cout<<"Total : $"<<subtotal+saletax<<endl;
+//	return 0;
+
+
+
+	
+//	double a=85.5,b=90.75,c=88.25;
+//	double ave= (a+b+c)/3;
+//	cout<<"The average of three numbers is :"<<setprecision(4)<<ave<<endl;
+
+
+
+
+	
+//int a,b;
+//cout<<"enter the numbers :"<<endl;
+//cout<<"number 1 :"<<endl;
+//cin>>a;
+//cout<<"number 2 :"<<endl;
+//cin>>b;
+//
+//int addition=a+b;
+//int subtraction=a-b;
+//int multiplication=a*b;
+//int division=a/b;
+//int moduls=a%b;
+//
+//cout<<"Addition\n\t"<<a<<"+"<<b<<"="<<addition<<endl;
+//cout<<"Subtraction\n\t"<<a<<"-"<<b<<"="<<subtraction<<endl;
+//cout<<"Multiplication\n\t"<<a<<"x"<<b<<"="<<multiplication<<endl;
+//cout<<"Division\n\t"<<a<<"/"<<b<<"="<<division<<endl;
+//cout<<"Moduls\n\t"<<a<<"%"<<b<<"="<<moduls<<endl;
+
+ 
+ 
+	
+//	double w,h,BMI;
+//	cout<<"Enter your weight (in kg):"<<endl;
+//	cin>>w;
+//	cout<<"Enter your height (in meter):"<<endl;
+//	cin>>h;
+//	BMI=w/(h*h);
+//	cout<<"Your BMI is :"<<BMI<<endl;
+//	
+	
+	
+//	cout<<"Trip cost Estimater\n---------------"<<endl;
+//	double d,fc,fe,tc;
+//	cout<<"Enter the distance(km) :";
+//	cin>>d;
+//	cout<<"Enter the fule cost(per liter) :";
+//	cin>>fc;
+//	cout<<"Enter the fule efficiency :";
+//	cin>>fe;
+//	tc=(d/fe)*fc;
+//	cout<<"Total trip cost Rs :"<<tc<<endl;
+
+
+	
+
+}
