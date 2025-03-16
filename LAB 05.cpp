91cb65b24ab1739b6a52416e66b95b09de8666e4
momnaImran726 <mominaahimran@gmail.com>
2025-03-16 13:11:44 +0500

    Add files via upload

---
+++ LAB 05.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+//	int a;
+//	cout<<" enter an integer : ";
+//	cin>>a;
+//	if(a>0)
+//	cout<<" POSITIVE number ";
+	
+//	int a;
+//	cout<<" Enter a number :";
+//	cin>>a;
+//	if(a%2==0){
+//		cout<<" The nunmber is even : ";
+//	} 
+	
+//	double a,b,c;
+//	cout<<" Enter three unmber to find the greatest one : ";
+//	cin>>a>>b>>c;
+//	if(a>b&&a>c){
+//		cout<<" The greatest nunmber is "<<a<<endl;
+//	}
+//	if(b>a&&b>c){
+//		cout<<" The greatest number is "<<b<<endl;
+//	}
+//	if(c>a&&c>b){
+//		cout<<" The greatset number is "<<c<<endl;
+//	}
+
+//	int g;
+//	cout<<" Enter yours marks out of 100 : ";
+//	cin>>g;
+//	if(g>=90){
+//		cout<<" congratulations!\n\t your grade is A+ ";
+//	}
+//	if(g<90&&g>=80){
+//		cout<<" congratulations!\n\t your grade is A  ";
+//	}
+//	if(g<80&&g>=70){
+//		cout<<" congratulations!\n\t your grade is B ";
+//	}
+//	if(g<70&&g>=60){
+//		cout<<" congratulations!\n\t your grade is C ";
+//	}
+//	if(g<60){
+//		cout<<" Alash !\n\t you are fail ";
+//	}
+	
+
+	
+//	char signal;
+//	cout<<" Enter any one (r,g,y) ";
+//	cin>>signal;
+//	if(signal=='r'){
+//		cout<<" the signal is red \n\t Stop ";
+//	}
+//	if(signal=='g'){
+//		cout<<" the signal is green \n\t Go ";
+//	}
+//	if(signal=='y'){
+//		cout<<" the signal is yellow \n\t Slow Down ";
+//	}
+		
+		
+	
+		
+		
+	
+//	char grade;
+//	int service,basic_salary,gross_salary,bonus,tax,net_salary;
+//	cout<<"Enter your grade :"<<endl;
+//	cin>>grade;
+//	cout<<"Enter years you have served :"<<endl;
+//	cin>>service;
+//	if(grade=='a')
+//	{
+//	basic_salary=50000;	
+//	}
+//	if(grade=='b')
+//	{
+//	basic_salary=30000;	
+//	}
+//	if(grade=='c')
+//	{
+//	basic_salary=20000;	
+//	}
+//	if(service>=10)
+//	{
+//		bonus=(10*basic_salary)/100;	
+//	}a
+//	if(service>=5 && service<=9)
+//	{
+//		bonus=(5*basic_salary)/100;
+//	}
+//	if(service<5)
+//	{
+//		bonus=0;
+//	}
+//	gross_salary=basic_salary + bonus;
+//	if(gross_salary<30000)
+//	{
+//		tax=0;
+//	}
+//	if(30000<=gross_salary&&gross_salary<=40000)
+//	{
+//		tax=10*gross_salary/100;
+//	}
+//	if(gross_salary>40000)
+//	{
+//		tax=20*gross_salary/100;
+//	}
+//	net_salary=gross_salary - tax;
+//cout<<"   Your basic salary : "<<basic_salary<<endl;
+//cout<<"   Your bonus is : "<<bonus<<endl;
+//cout<<"   Your gross salary is : "<<gross_salary<<endl;
+//cout<<"   Tax deducted : "<<tax<<endl;
+//cout<<"   Net salary  : "<<net_salary;
+
+
+
+
+
+
+
+//	cout<<"\n------WELCOM TO THE ATM------\n";
+//	int balance,withdrawal,amountLeft;
+//	cout<<"\nEnter your total accout balanced :\n";
+//	cin>>balance;
+//	cout<<"\nEnter amount you want to withdraw :\n";
+//	cin>>withdrawal;
+//	if(withdrawal<=balance){
+//		amountLeft= balance-withdrawal;
+//		cout<<"\n your remaining blance is :"<<amountLeft<<endl;
+//	}
+//	else
+//	cout<<"\nIncufficient balance.\n";
+
+	
+	
+	
+	
+
+	
+	
+//	int salary,experience;
+//	cout<<"Enter you salary :";
+//	cin>>salary;
+//	cout<<"Enter you work experience :";
+//	cin>>experience;
+//	if((salary>=25000)||(salary>=20000&&experience>5))
+//	{
+//		cout<<"You are eligible for loan";	
+//	}
+//	else
+//	cout<<"You are not eligible for loan";
+//	
+	
+
+}
