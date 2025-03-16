ec44f411cd4c524b40be78936779f8d247cd6e29
momnaImran726 <mominaahimran@gmail.com>
2025-03-16 14:54:54 +0500

    Add files via upload

---
+++ LAB 01.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+//                     ----------- 01----------
+	
+//	cout<<"Item\tPrice\tQuality\n";
+//	cout<<"Pen\t$1\t10\n";
+//	cout<<"Pencil\t$2\t20\n";
+//	cout<<"Eraser\t$3\t30\n";
+
+
+	
+//	                   ----------- 02 -----------
+
+
+
+//  cout<<"She said,\ Let's learn C++!\"\n";
+//  cout<<"To start save your file in C:\\C++_Projects\\Lab1\n";
+//  cout<<"Good Luck!\n";
+	
+//	                   
+
+
+//	                        ---------------03----------------
+
+
+//	cout<<"**********\n";
+//	cout<<"*\t *\n";
+//	cout<<"*\t *\n";
+//	cout<<"*\t *\n";
+//	cout<<"**********\n";
+
+
+//                    ------------04-----------------
+
+
+//   cout<<"-----------\n|         |\n|  45+78  |\n|  = 123  |\n|         |\n-----------";	
+
+
+
+//                      ------------05-------------
+
+//    cout<<"\tReturant XYZ\n";
+//	cout<<"Item\tquantity    price\n";
+//	cout<<"Pizza\t2          $15\n";
+//	cout<<"Pasta\t1          $12\n";
+//	cout<<"Soda\t3          $3\n";
+//	cout<<"Total              $51\n";
+  
+//                    --------------- 06------------------
+
+//    cout<<"\t\"The Road Not Taken\"\n";
+//	cout<<"\t  by Robert Frost\n";
+//	cout<<"Two roads diverged in a yellow wood,\n";
+//	cout<<"And sorry i could not travel both\n";
+//	cout<<"And be one taveler, long i started\n";
+//	cout<<"To where it bent in the undergrowth\n";
+
+//                      -----------07------------
+
+//	cout<<"Person A: \"Hello! How are you?\n";
+//	cout<<"Person B: \"I am fine, Thank you! How about you?\n";
+//	cout<<"Person B: \"I am doing great. Thanks for asking!\n";
+
+
+
+	return 0;
+}
