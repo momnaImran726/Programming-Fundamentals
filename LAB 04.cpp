f4b8cd9a9f31a310c4ab7a47baca877af8c9a472
momnaImran726 <mominaahimran@gmail.com>
2025-03-16 13:14:49 +0500

    Add files via upload

---
+++ LAB 04.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	
+	
+//	int a;
+//	cout<<"enter the number :";
+//	cin>>a;
+//	(a%2==0)? cout<<"The number"<<a<<" is even": cout<<"The number "<<a<<" is odd";
+	
+	
+	
+//	int age;
+//	cout<<"Enter your age :";
+//	cin>>age;
+//	(age<13)? cout<<"you are child":(age>19)?cout<<"you are adult": cout<<"you are teenager"; 
+	
+	
+	
+	
+//	cout<<" WELLCOM TO THE BANK OF SOFTWARE : ";
+//	int x;
+//    cout<<setfill('*')<<setw(100);
+//	cout<<"Enter your account balnce:";
+//	cin>>x;
+//	cout<<setfill('*')<<setw(70)<<endl;
+//	cout<<"\n";
+//	(x<100)? cout<<"your account is low balance account":(x>500)?cout<<"your account is premium account": cout<<"your account is Standard account*"<<endl;
+//	cout<<setfill('*')<<setw(70)<<endl;
+//	cout<<""<<endl;
+//	bool y;
+//	cout<<"Enter 1 for loyal and 0 for non loyal  ";
+//	cin>>y;
+//	cout<<setw(110)<<setfill('*')<<setw(110)<<setfill('*')<<endl;
+//	(x>200)&&(y==1)? cout<<"you are elligible for special offer":cout<<"\nyou are not elligeble\n";
+//	cout<<setw(70)<<setfill('*')<<"\n"<<setw(50)<<setfill('*')<<endl;
+//	
+
+
+	
+	
+	
+	
+}
