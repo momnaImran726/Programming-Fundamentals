805060725c9a1581da463fa4ae92efc3aad9ef94
momnaImran726 <mominaahimran@gmail.com>
2025-03-16 14:53:42 +0500

    Add files via upload

---
+++ LAB 02.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//	          TASK1
+	
+//	int num1, num2;
+//	cout << "Enter two numbers:\n";
+//	cout << "Number1: ";
+//	cin >> num1;
+//	cout << "Number2: ";
+//	cin >> num2;
+//	int addition = num1 + num2;
+//	int subtraction = num1 - num2;
+//	int multiplication = num1 * num2;
+//	int division = num1 / num2;
+//	int modulus = num1 % num2;
+//	cout << "Addition: " << num1 << " + " << num2 << " = " << addition << endl;
+//	cout << "Subtraction: " << num1 << " - " << num2 << " = " << subtraction << endl;
+//	cout << "Mutliplication: " << num1 << " * " << num2 << " = " << multiplication << endl;
+//	cout << "Division: " << num1 << " / " << num2 << " = " << division << endl;
+//	cout << "Modulus: " << num1 << " % " << num2 << " = " << modulus << endl;
+
+//            TASK2
+	
+//	double weight, height, bmi;
+//	cout << "BMI Calculator\n";
+//	cout << "Enter your weight in kilograms: ";
+//	cin >> weight;
+//	cout << "Enter your height in meters: ";
+//	cin >> height;
+//	bmi = weight / (height * height);
+//	cout << fixed << setprecision(4);
+//	cout << "Your BMI is: " << bmi << endl;
+	
+//	           TASK3
+	
+//    double x1 = 3, y1 = 4, x2 = 6, y2 = 8;
+//	double squared_distance = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+//	cout << "The squared distance between the points ("<< x1 << ", " << y1 << ") and (" << x2 << ", " << y2 <<") is: " << squared_distance <<endl;
+
+//             TASK4
+
+//   int a;
+//	cout << "enter an integer"<<endl;
+//	cin >> a;
+//	if(a>0)
+//	{
+//		cout << "a is positive"<<endl;
+//	}
+//	else if(a<0)
+//	{
+//		cout << "a is negative"<<endl;
+//	}
+//	else
+//	{
+//		cout << "a is equal to zero"<<endl;
+//	}
+
+//           TASK5
+
+//#include<iostream
+//#include<iomanip>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string username;
+//	int password;
+//	cout << "Enter username and password"<<endl;
+//	getline(cin,username);
+//	cin >> password;
+//	if(username=="admin" && password==12345)
+//	{
+//		cout << "Access Granted"<<endl;
+//	}
+//	else if(username=="admin" && password!=12345)
+//	{
+//		cout << "Wrong Password" <<endl;
+//	}
+//	else
+//	{
+//		cout << "User Not Found" <<endl;
+//	}
+//	return 0;
+//}
+
+
+
